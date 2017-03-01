34fcc9317a7c6157e4820c56d6ec0f058e3950be
Thomas Conner <thomas@kinvey.com>
2017-03-01 11:06:57 -0500

    Updates

--- webpack.config.js
+++ webpack.config.js
@@ -2,26 +2,22 @@
 var path = require('path');
 var webpack = require('webpack');
 var pkg = require('./package.json');
-var BANNER = '/**\n'
-  + ' * @preserve\n'
-  + ' * ' + pkg.name + ' v' + pkg.version + '\n'
-  + ' * ' + pkg.description + '\n'
-  + ' * ' + pkg.homepage + '\n'
-  + ' *\n'
-  + ' * Copyright (c) 2017, ' + pkg.author + '.\n'
-  + ' * All rights reserved.\n'
-  + ' *\n'
-  + ' * Released under the ' + pkg.license + ' license.\n'
-  + ' */\n';
+var BANNER = '@preserve\n'
+  + pkg.name + ' v' + pkg.version + '\n'
+  + pkg.description + '\n'
+  + pkg.homepage + '\n'
+  + '\n'
+  + 'Copyright (c) 2017, ' + pkg.author + '.\n'
+  + 'All rights reserved.\n'
+  + '\n'
+  + 'Released under the ' + pkg.license + ' license.'
 
 module.exports = {
-  devtool: 'source-map',
-  entry: ['./src/kinvey.ts'],
-  externals: [/^\@angular\//, /^rxjs\//],
+  entry: './src/index.js',
   module: {
-    rules: [
-      { test: /\.json$/, use: ['json-loader'] },
-      { test: /\.ts$/, use: ['awesome-typescript-loader'] }
+    loaders: [
+      { test: /\.json$/, loader: 'json-loader' },
+      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' }
     ]
   },
   output: {
@@ -31,13 +27,11 @@ module.exports = {
     path: path.resolve(__dirname, 'dist')
   },
   plugins: [
-    new webpack.BannerPlugin({ banner: BANNER, raw: true }),
+    new webpack.BannerPlugin(BANNER, { raw: false, entryOnly: true }),
     new webpack.NormalModuleReplacementPlugin(
       /kinvey-js-sdk\/dist\/identity\/src\/popup\.js/,
       require.resolve(path.resolve(__dirname, 'node_modules/kinvey-phonegap-sdk/dist/popup.js'))
     )
   ],
-  resolve: {
-    extensions: ['.ts', '.js']
-  },
+  profile: true
 };
