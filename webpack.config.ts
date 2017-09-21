10e9043914609a0ac6f4d4f389672194cac85ad3
Thomas Conner <thomas@kinvey.com>
2017-09-21 12:01:49 -0400

    Rename webpack config file

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,8 +1,6 @@
 import * as webpack from 'webpack';
 import * as path from 'path';
 import * as fs from 'fs';
-import * as angularExternals from 'webpack-angular-externals';
-import * as rxjsExternals from 'webpack-rxjs-externals';
 
 const pkg = JSON.parse(fs.readFileSync('./package.json').toString());
 
@@ -31,9 +29,6 @@ export default {
             options: {
               configFileName: 'tsconfig.json'
             }
-          },
-          {
-            loader: 'angular2-template-loader'
           }
         ],
         exclude: [
@@ -49,22 +44,16 @@ export default {
     ]
   },
   plugins: [
-    new webpack.ContextReplacementPlugin(
-      /angular(\\|\/)core(\\|\/)@angular/,
-      path.join(__dirname, 'src')
-    ),
-
     new webpack.optimize.UglifyJsPlugin({
       include: /\.min\.js$/,
       sourceMap: true
     }),
-
     new webpack.BannerPlugin({
       banner: `
 /**
  * ${pkg.name} - ${pkg.description}
  * @version v${pkg.version}
- * @author ${pkg.author.name}
+ * @author ${pkg.author}
  * @link ${pkg.homepage}
  * @license ${pkg.license}
  */
@@ -72,6 +61,5 @@ export default {
       raw: true,
       entryOnly: true
     })
-
   ]
 } as webpack.Configuration;
