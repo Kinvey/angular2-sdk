09fced744cabbeed8dea4f0675c777f42aabae5f
Thomas Conner <thomas@kinvey.com>
2017-09-21 12:01:22 -0400

    Consolidate scripts

--- scripts/copy-definitions.js
+++
@@ -1,3 +0,0 @@
-const fs = require('fs');
-const file = fs.readFileSync('src/kinvey.d.ts').toString();
-fs.writeFileSync('dist/kinvey-angular2-sdk.d.ts', file);
--- scripts/copy-readme.js
+++
@@ -1,3 +0,0 @@
-const fs = require('fs');
-const file = fs.readFileSync('README.md').toString();
-fs.writeFileSync('dist/README.md', file);
